4755dece5e2175ff292d4fe776fad9353d2c28ce
Jonnie Spratley <jonnie.spratley@ge.com>
2017-09-07 14:51:05 -0700

    updated code

--- app/Component/SimpleInlineToolbarEditor.js
+++ app/Component/SimpleInlineToolbarEditor.js
@@ -1,53 +1,47 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Editor, { createEditorStateWithText } from 'draft-js-plugins-editor'; // eslint-disable-line import/no-unresolved
-import  EditorState from 'draft-js';
+import Editor, {createEditorStateWithText} from 'draft-js-plugins-editor'; // eslint-disable-line import/no-unresolved
+import EditorState from 'draft-js';
 import createInlineToolbarPlugin from 'draft-js-inline-toolbar-plugin';
-import 'draft-js-inline-toolbar-plugin/lib/plugin.css';
+
+// TODO: Styles
+//import 'draft-js-inline-toolbar-plugin/lib/plugin.css';
 import editorStyles from '../../public/editorStyles.css';
-import toolbarStyle from '../../draft-js-inline-toolbar-plugin/lib-css/toolbarStyles.css';
-import toolbarStyles from '../../draft-js-inline-toolbar-plugin/src/toolbarStyles.css';
-import buttonStyles from '../../draft-js-inline-toolbar-plugin/lib-css/buttonStyles.css';
-
-
+//import toolbarStyle from '../../draft-js-inline-toolbar-plugin/lib-css/toolbarStyles.css';
+//import toolbarStyles from '../../draft-js-inline-toolbar-plugin/src/toolbarStyles.css';
+//import buttonStyles from '../../draft-js-inline-toolbar-plugin/lib-css/buttonStyles.css';
 
 const inlineToolbarPlugin = createInlineToolbarPlugin();
-const { InlineToolbar } = inlineToolbarPlugin;
+const {InlineToolbar} = inlineToolbarPlugin;
 const plugins = [inlineToolbarPlugin];
-const text = 'In this editor a toolbar shows up once you select part of the text …';
+const text = 'In this editor a toolbar shows up once you select part of the text ...';
 
+export default class SimpleInlineToolbarEditor extends React.Component {
+	constructor(props) {
+		super(props);
 
-class MyEditor extends React.Component{
+		this.state = {
+			editorState: createEditorStateWithText(text)
+		};
 
+		this.onChange = (editorState) => this.setState({editorState});
 
-  constructor(props) {
-    super(props);
-    this.state = {editorState: createEditorStateWithText(text)};
-    this.onChange = (editorState) => this.setState({editorState});
-    focus = () => {
-    this.editor.focus();
-  };
-  }
-  render() {
-    return (
-    
-     <div className='container-fluid editor' onClick={this.focus}>
-       
-      <Editor 
-          editorState={this.state.editorState}
-          onChange={this.onChange}
-          plugins={plugins}
-          ref={(element) => { this.editor = element; }}
-        />
-
-        <InlineToolbar />
-        </div>  
-     
-    );
-  }
+		focus = () => {
+			this.editor.focus();
+		};
+	}
+	render() {
+		return (
+			<div className='container-fluid'>
+				<div className='editor' onClick={this.focus}>
+					<Editor editorState={this.state.editorState} onChange={this.onChange} plugins={plugins} ref={(element) => {
+						this.editor = element;
+					}}/>
+					<InlineToolbar/>
+				</div>
+			</div>
+		);
+	}
 };
 
-ReactDOM.render(
-  <MyEditor />,
-  document.getElementById('app')
-);
+//Shouldnt you just export the module cuz app.js is rendering?
+//ReactDOM.render(<SimpleInlineToolbarEditor/>, document.getElementById('app'));
--- app/app.js
+++ app/app.js
@@ -1,14 +1,5 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import {Editor, EditorState} from 'draft-js';
-// //import Editor from 'draft-js-plugins-editor'; // eslint-disable-line import/no-unresolved
-// import createInlineToolbarPlugin from 'draft-js-inline-toolbar-plugin'; 
-// import SimpleInlineToolbarEditor from './Children/SimpleInlineToolbarEditor'; 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SimpleInlineToolbarEditor from './Component/SimpleInlineToolbarEditor';
 
-// const inlineToolbarPlugin = createInlineToolbarPlugin();
-
-
-// ReactDOM.render(
-//   <SimpleInlineToolbarEditor />,
-//   document.getElementById('app')
-// );
+ReactDOM.render(<SimpleInlineToolbarEditor />, document.getElementById('app'));
